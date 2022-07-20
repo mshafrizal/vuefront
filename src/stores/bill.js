3a088a14f2b9b8767acb332b320844ed4dbe8a67
Muhammad Shafrizal <mshafrizal@gmail.com>
2022-07-20 11:05:45 +0700

    Bill view

--- src/stores/bill.js
+++ src/stores/bill.js
@@ -3,7 +3,32 @@ import { defineStore } from 'pinia'
 export const useBillStore = defineStore({
   id: 'bill',
   state: () => ({
-    bills: []
+    bills:  [
+        {
+            adminFee: "0.0",
+            billid: 8,
+            currency: 360,
+            body: [
+                "DENOM: 50000"
+            ]
+        },
+        {
+            adminFee: "0.0",
+            billid: 8,
+            currency: 360,
+            body: [
+                "DENOM: 100000"
+            ]
+        },
+        {
+            adminFee: "0.0",
+            billid: 8,
+            currency: 360,
+            body: [
+                "DENOM: 150000"
+            ]
+        },
+    ]
   }),
   getters: {
     filteredBill: (state) => {
@@ -11,37 +36,11 @@ export const useBillStore = defineStore({
             const body = bill.body[0].split(':')
             const denom = parseInt(body[1])
             return denom >= 100000
+        }).map(bill => {
+            const body = bill.body[0].split(':')
+            const denom = parseInt(body[1])
+            return denom
         })
     }
-  },
-  actions: {
-    getBills() {
-        this.bills = [
-            {
-                adminFee: "0.0",
-                billid: 8,
-                currency: 360,
-                body: [
-                    "DENOM: 50000"
-                ]
-            },
-            {
-                adminFee: "0.0",
-                billid: 8,
-                currency: 360,
-                body: [
-                    "DENOM: 100000"
-                ]
-            },
-            {
-                adminFee: "0.0",
-                billid: 8,
-                currency: 360,
-                body: [
-                    "DENOM: 150000"
-                ]
-            },
-        ]
-    },
   }
 })
---
+++ src/views/BillView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <p>Original</p>
+        <pre>{{ JSON.stringify(originalBills, null, 2)}}</pre>
+
+        <p>Filtered</p>
+        <pre>{{ JSON.stringify(filterdBills, null, 2)}}</pre>
+    </div>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+import { useBillStore } from '../stores/bill'
+
+export default {
+    setup() {
+        const billStore = useBillStore()
+        const originalBills = billStore.bills
+        const filterdBills = billStore.filteredBill
+
+        return {
+            originalBills,
+            filterdBills
+        }
+    }
+}
+</script>
